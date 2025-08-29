1cf520c7a9936764b165a2b3535c4c0829bc2327
Jblue254 <japhethkiprono2020@gmail.com>
2025-08-29 13:47:48 +0300

    capital

--- Week 1 React/vite.config.js
+++ Week 1 React/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/week1-react/",
+  base: "/Week-1-React/",
   plugins: [react() , tailwindcss(),],
 })
